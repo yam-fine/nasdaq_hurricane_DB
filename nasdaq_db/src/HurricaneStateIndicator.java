b8572558fbd613ddbab33e4233d749fb280d2810
yamdeking <yamf45@gmail.com>
2021-09-05 18:09:23 +0300

    standard div + expected values

--- nasdaq_db/src/HurricaneStateIndicator.java
+++ nasdaq_db/src/HurricaneStateIndicator.java
@@ -1,14 +1,5 @@
-/**
- * class holds final data we will use to calculate different kinds of indicators
- */
 public class HurricaneStateIndicator {
-	/**
-	 * state that the hurricane passed through
-	 */
 	String stateName;
-	/**
-	 * the change in the following quarter
-	 */
 	float change;
 	HurricaneStateIndicator(String name , float num){
 		stateName = name;
