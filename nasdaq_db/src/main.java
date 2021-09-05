1ab47cf8dbf8106102d01ac84ce0d17f656ddbcd
yamdeking <yamf45@gmail.com>
2021-09-05 18:09:18 +0300

    standard div + expected values

--- nasdaq_db/src/main.java
+++ nasdaq_db/src/main.java
@@ -1,13 +1,10 @@
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class main {
 	public static void main(String[] args) throws ParseException {
 //		System.out.println("Hello world!");
 		ExcelFileReader Fr = new ExcelFileReader();
-		HashMap<Integer, dateData> dD = Fr.readStockDataFile("nasdaq_db/seconedchance.xlsx");
-		HashMap<String, ArrayList<hurricaneData>> hD = Fr.readHurricanekDataFile("nasdaq_db/hurricaneDB" + ".xlsx");
-		DataAnalayzer dA = new DataAnalayzer(hD,dD);
+//		Fr.readStockDataFile("path");
+//		Fr.readHurricanekDataFile("path",34);
 	}
 }
